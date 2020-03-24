6476c08a526e54bd96bc58b0574f06dfc15bc7fc
alexfaucheux <44651995+alexfaucheux@users.noreply.github.com>
2020-03-23 23:31:30 -0500

    Database update

--- capstone_web_app/Stargazer_app.py
+++ capstone_web_app/Stargazer_app.py
@@ -1,6 +1,7 @@
 from app import app, db
-from app.models import User, Image, Weather
+from app.models import User, Image, Weather, ObjectOfInterest
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Image': Image, 'Weather': Weather}
+    return {'db': db, 'User': User, 'Image': Image, 'Weather': Weather, 'Ob': ObjectOfInterest}
