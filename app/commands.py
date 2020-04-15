5d434c3afbca68081f330c0949e4587e8c62ba32
alexfaucheux <44651995+alexfaucheux@users.noreply.github.com>
2020-04-15 10:37:34 -0500

    potential fix for the pictures

--- app/commands.py
+++ app/commands.py
@@ -1,9 +1,25 @@
 import click
 from flask.cli import with_appcontext
+from flask import url_for
 from app.extensions import db
+
 
 
 @click.command(name='create_tables')
 @with_appcontext
 def create_tables():
     db.create_all()
+
+
+@click.command(name='fill_image_table')
+@with_appcontext
+def fill_image_table():
+    from app.models import Image
+    images = ["images/img-1.jpg", "images/img-2.jpg", "images/img-3.jpg", "images/img-4.jpg",
+              "images/img-5.jpg", "images/meteor.jpg", "images/moon.jpg", "images/stars.jpg"]
+
+    for i in range(len(images)):
+        img_obj = Image(image_name="Image {}".format(i), image_url=url_for('static', filename=images[i]))
+        db.session.add(img_obj)
+
+    db.session.commit()
